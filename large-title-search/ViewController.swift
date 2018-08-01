9a893ff626ccab999d7474653e25583cc3388ed9
Athyla <adolfo.athyla@gmail.com>
2018-08-01 13:21:42 -0300

    Initial Commit

---
+++ large-title-search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  large-title-search
+//
+//  Created by Cmdev on 01/08/2018.
+//  Copyright © 2018 PosCash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
